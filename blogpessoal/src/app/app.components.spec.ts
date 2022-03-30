b846d93d6793b976c42bed34e43f8ca5b908673c
Suelen Bento <98328369+SuelenBento@users.noreply.github.com>
2022-03-30 16:42:06 -0300

    menu

--- blogpessoal/src/app/app.components.spec.ts
+++ blogpessoal/src/app/app.components.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blogpessoal'`, () => {
+  it(`should have as title 'blogpantera'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blogpessoal');
+    expect(app.title).toEqual('blogpantera');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('blogpessoal app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('blogpantera app is running!');
   });
 });
--- blogpessoal/src/app/app.module.ts
+++ blogpessoal/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
